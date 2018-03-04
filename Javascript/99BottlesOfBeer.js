fc5bda8d7205c93fd523228aa7b05fa5d50e7364
Sterling Chin <schin@overstock.com>
2018-03-03 23:29:42 -0700

    update 99bottles

--- Javascript/99BottlesOfBeer.js
+++ Javascript/99BottlesOfBeer.js
@@ -2,25 +2,27 @@
 
 const beer = () => {
     for(var i = 99; i > 0 ; i--) {
-      if(i > 1) {
-        console.log(i + ' bottles of beer on the wall,');
-        console.log(i + ' bottles of beer.');
-        console.log('Take one down, pass it around,');
-        if(i !== 2) {
-          console.log((i-1) + ' bottles of beer on the wall.');
-        } else {
-          console.log((i-1) + ' bottle of beer on the wall.');
+        if(i > 1) {
+            console.log(i + ' bottles of beer on the wall,');
+            console.log(i + ' bottles of beer.');
+            console.log('Take one down, pass it around,');
+            if(i !== 2) {
+              console.log((i-1) + ' bottles of beer on the wall.');
+              console.log(' ');
+            } else {
+              console.log((i-1) + ' bottle of beer on the wall.');
+              console.log(' ');
+            }
         }
-      }
-      if(i === 1) {
-        console.log(i + ' bottle of beer on the wall,');
-        console.log(i + ' bottle of beer.');
-        console.log('Take one down, pass it around,');
-        console.log('No more bottles of beer on the wall!');
-      }
-      return 'Now, sit down, call and Uber, and get home safe!';
+        if(i === 1) {
+            console.log(i + ' bottle of beer on the wall,');
+            console.log(i + ' bottle of beer.');
+            console.log('Take one down, pass it around,');
+            console.log('No more bottles of beer on the wall!');
+        }
     }
-  };
+    return 'Now, sit down, call and Uber, and get home safe!';
+};
 
 beer();
 
