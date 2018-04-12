f85f3b9fceda8774944d193d296369e0c107626c
Sterling Chin <schin@overstock.com>
2018-04-11 23:03:27 -0600

    update and add capitalization.js

---
+++ Javascript/capitalization.js
@@ -0,0 +1,21 @@
+// Write a function that capitalizes the first letter of every word in the string and accepts a second argument a list of exceptions.
+
+// The first word of every title should ALWAYS have a capital leter.
+
+// E.g. ('pizza is good', ['is']) --> 'Pizza is Good'
+
+const capitalization = (n, x) => {
+    return n.split(' ').map(word => {
+        if(x.indexOf(word) < 0){
+            return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+        }
+        return word;
+    }).join(' ');
+};
+   
+const str1 = 'something like this is going to work if i write javascript correctly';
+
+console.log(capitalization(str1, ['like', 'going','write', 'correctly']));
+capitalization('pizza is good', ['is']);
+
+//See the code in action here: https://repl.it/@SterlingChin/capitalization
--- Javascript/reversePolish.js
+++ Javascript/reversePolish.js
@@ -2,31 +2,33 @@
 // Your program should support positive and negative integers and the operators +, -, *.
 
 const reversePolishNotation = (n) => {
-    let array = n.split(' ');
-    let answer = 0;
-    const operators = ['+', '-', '*', '/'];
-    const loop = x => {
-      for(let i = 0; i < x.length; i++) {
-        const job = {
-          '+': parseInt(x[i-2]) + parseInt(x[i-1]),
-          '-': parseInt(x[i-2]) - parseInt(x[i-1]),
-          '*': parseInt(x[i-2]) * parseInt(x[i-1]),
-          '/': parseInt(x[i-2]) / parseInt(x[i-1])
-        };
-        if(operators.indexOf(array[i]) >= 0){
-          x.splice(i-2, 3, job[array[i]]);
-          if(array.length > 2){
-            loop(x);
-          }
-          answer = x[0];
+  let array = n.split(' ');
+  let answer = 0;
+  const operators = ['+', '-', '*', '/'];
+  const loop = x => {
+    for(let i = 0; i < x.length; i++) {
+      const job = {
+        '+': parseInt(x[i-2]) + parseInt(x[i-1]),
+        '-': parseInt(x[i-2]) - parseInt(x[i-1]),
+        '*': parseInt(x[i-2]) * parseInt(x[i-1]),
+        '/': parseInt(x[i-2]) / parseInt(x[i-1])
+      };
+      if(operators.indexOf(array[i]) >= 0){
+        x.splice(i-2, 3, job[array[i]]);
+        if(array.length > 2){
+          loop(x);
         }
+        answer = x[0];
       }
-    };
-    if(array.length > 2){
-      loop(array);
     }
-    return answer;
   };
-  
-  console.log(reversePolishNotation('15 7 1 1 + - / 3 * 2 1 1 + + -'), 'expect 5');
-  console.log(reversePolishNotation('9 3 4 + -'), 'expect 2');
+  if(array.length > 2){
+    loop(array);
+  }
+  return answer;
+};
+
+console.log(reversePolishNotation('15 7 1 1 + - / 3 * 2 1 1 + + -'), 'expect 5');
+console.log(reversePolishNotation('9 3 4 + -'), 'expect 2');
+
+//See the code in action here: https://repl.it/@SterlingChin/reversePolishNotation
